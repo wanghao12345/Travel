28a67f1b2bc20d6325ce21e202660110833ddb11
winner <1750110604@qq.com>
2018-05-30 11:32:38 +0800

    地区选择数据绑定

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,13 +1,49 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+    <city-list :cities="cities" :hot="hotCities"></city-list>
+    <city-alphabel :cities="cities"></city-alphabel>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabel from './components/Alphabel'
 export default{
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabel
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      console.log(res)
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
   }
 }
 </script>
---
+++ src/pages/city/components/Alphabel.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="list">
+    <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+  </ul>
+</template>
+
+<script>
+export default{
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    list-style-type: none;
+    .item
+      text-align: center
+      line-height: .4rem
+      color: $bgColor
+      font-size: .25rem
+</style>
