53d4455f2079fc03acfd54ff0c9652b02b426ba1
winner <1750110604@qq.com>
2018-06-01 14:55:39 +0800

    keep-alive的使用

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- keep-alive标签包裹的路由，当有过一次的请求过后，就不会再次去请求数据 -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -24,20 +25,33 @@ export default {
     HomeRecommend,
     HomeWeekend
   },
-  mounted () {
-    this.getHomeInfo()
-  },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
+  },  
+  mounted () {
+    this.lastCity = this.city
+    this.getHomeInfo()
+  },
+  computed: {
+    ...mapState(['city'])
+  },
+  // 使用keep-alive后，如果想重新请求数据，就可以使用该生命周期函数
+  activated () {
+    // 判断当前的城市和上次的是否相同，相同不执行，不相同则执行
+    if (this.lastCity != this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()      
+    }
   },
   methods: {
     getHomeInfo: function () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city='+this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc: function (res) {
