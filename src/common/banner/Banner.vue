70bc828d90caa9bee7977bb8e92ad1c1d0a6435b
winner <1750110604@qq.com>
2018-06-06 17:25:36 +0800

    修改Gallary的bug

---
+++ src/common/banner/Banner.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide>
+          <img class="gallary-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg">
+        </swiper-slide>
+        <swiper-slide>
+          <img class="gallary-img" src="http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png">
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParants: true,
+        observe: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-container
+    overflow: inherit
+  .swiper-pagination >>> span
+    font-size: .2rem
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      width: 100%
+      heigth: 0
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        position: absolute
+        bottom: -1rem
+        color: #fff
+        font-size: .2rem
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
 import Detail from '@/pages/detail/Detail'
+import Banner from 'common/banner/Banner'
 
 Vue.use(Router)
 
@@ -20,6 +21,10 @@ export default new Router({
       path: '/detail/:id',
       name: 'Detail',
       component: Detail
+    }, {
+      path: '/banner',
+      name: 'Banner',
+      component: Banner
     }],
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 }
