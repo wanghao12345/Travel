59a3b3af9bafea2c08c8c0294a85a439bf6fc509
winner <1750110604@qq.com>
2018-06-07 13:49:51 +0800

    添加定位信息

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper :list="swiperList"></home-swiper>
     <home-icons :list="iconList"></home-icons>
+    <home-location></home-location>
     <home-recommend :list="recommendList"></home-recommend>
     <home-weekend :list="weekendList"></home-weekend>
   </div>
@@ -12,6 +13,7 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeLocation from './components/Location'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
@@ -22,6 +24,7 @@ export default {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
+    HomeLocation,
     HomeRecommend,
     HomeWeekend
   },
---
+++ src/pages/home/components/Location.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="location-icon">
+      <div class="iconfont">&#xe611;</div>
+    </div>
+    <div class="location-info">
+      定位失败
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeLocation'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container
+    width: 100%;
+    height: 1rem;
+    overflow: hidden;
+    border-top: 1px solid #ccc;
+    margin-top: .5rem;
+    .location-icon
+      float: left
+      width: .5rem
+      height: 100%
+      line-height: 1rem
+      .iconfont
+        text-align: center
+    .location-info
+      float: left
+      min-width: 1rem
+      height: 100%
+      font-size: .2rem
+      line-height: 1rem
+      text-align: center
+</style>
